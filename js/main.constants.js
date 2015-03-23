59565e060994139bf31f43c5d828717358ab00a4
Brandon Espinoza <espinozab100@gmail.com>
2015-03-23 17:12:29 -0500

    Login and register works and also modal pops up. Working on apending to the table

---
+++ js/main.constants.js
@@ -0,0 +1,16 @@
+angular
+  .module('tas')
+  .constant('BASE_URL', 'https://tangularlist.firebaseio.com')
+  .constant('COHORT_OPTIONS', [
+    'Full Stack Developer',
+    'Web Designer',
+    'N/A',
+    'Back End Developer',
+    'Front End Developer',
+    'Teacher',
+    'Window Washer',
+    'Electrician',
+    'Plumber',
+    'Barber',
+    'Hair Specialists'
+  ]);
